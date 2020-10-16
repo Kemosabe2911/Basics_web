3aec9f6eed8974e8cf8d14fcb02aa3e6f7971ecb
Kemosabe2911 <stevinprince29@gmail.com>
2020-10-16 15:57:31 +0530

    Upload complete display err

--- imag-upload/server.js
+++ imag-upload/server.js
@@ -12,6 +12,37 @@ const pool = new Pool({
     database: 'image_up',
     password: 'Stevin123',
     port: 5432,
-  })
+  });
+
+const PORT = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './images/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname);
+    }
+  });
+
+  var upload = multer({ storage: storage });
+
+app.use(express.static(__dirname + '/images'));
+
+app.post('/',upload.single('image_upload'),(req,res)=>{
+    var image= req.file.filename;
+    var imagePath = req.file.path;
+    console.log(image);
+    console.log(imagePath);
+  res.render('index',{image: imagePath});
+}); 
+
+app.listen(PORT, () =>{
+    console.log(`Server Running on port ${PORT}`);
+});
